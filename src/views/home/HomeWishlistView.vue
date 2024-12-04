2aac731b574c85a3a8a04495b4467ce97f0b18a9
sw1kwon <kwonsoonwon0628@gmail.com>
2024-12-04 21:24:58 +0900

    Add views/home/HomeWishlistView.vue

---
+++ src/views/home/HomeWishlistView.vue
@@ -0,0 +1,18 @@
+<template>
+  <MovieWishlist />
+</template>
+
+<script>
+import MovieWishlist from "@/components/home-wishlist/MovieWishlist.vue";
+
+export default {
+  name: "HomeWishlist",
+  components: {
+    MovieWishlist,
+  },
+};
+</script>
+
+<style scoped>
+/* 현재 CSS 파일이 비어 있으므로 스타일이 필요할 경우 여기에 추가하세요. */
+</style>
