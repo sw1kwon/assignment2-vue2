aaa0b99677ce307b858b61a594e5d39bf3527775
sw1kwon <kwonsoonwon0628@gmail.com>
2024-12-04 12:05:47 +0900

    Update deploy.yml & vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:
-      process.env.BRANCH === 'develop'
-          ? '/assignment2-vue2/develop/' // develop 브랜치: /develop/ 경로로 배포
-          : '/assignment2-vue2/',        // main 브랜치: 루트 경로로 배포
+  publicPath: '/assignment2-vue2/', // GitHub Pages 경로에 맞게 고정
 });
