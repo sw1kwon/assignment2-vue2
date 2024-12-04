f4bcc5e42df37ee44500d571bd645d9cfb94adab
sw1kwon <kwonsoonwon0628@gmail.com>
2024-12-04 11:40:29 +0900

    Add deploy.yml & Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/assignment2-vue2/' : '/',
-})
+  publicPath:
+      process.env.BRANCH === 'develop'
+          ? '/assignment2-vue2/develop/' // develop 브랜치: /develop/ 경로로 배포
+          : '/assignment2-vue2/',        // main 브랜치: 루트 경로로 배포
+});
